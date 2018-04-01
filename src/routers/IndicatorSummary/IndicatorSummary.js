323e1518499dc14a4dc382e35c0a6b961da00575
wanglizhuo <wanglizhuojoyce@qq.com>
2018-04-01 16:01:00 +0800

    adding

--- src/routers/IndicatorSummary/IndicatorSummary.js
+++ src/routers/IndicatorSummary/IndicatorSummary.js
@@ -9,14 +9,33 @@ class IndicatorSummary extends PureComponent {
     this.state = {}
   }
   componentDidMount () {
-
+    const $ = window.$
+    console.log($('#rootId'))
   }
 
   render () {
     return (
       <Layout className="indicator_summary">
-        <div>
-
+        <div id="rootId">
+          <div className="btn">
+            <button className="more">加载更多</button>
+          </div>
+            <ul className="lists">
+                <li className="item">
+                    <img src="" alt="" className="itemImg"/>
+                    <div className="itemContent">
+                      <p className="itemTitle">标题</p>
+                      <p className="itemDesc">内容</p>
+                      <p className="itemAddress">
+                          <label htmlFor="" className="itemAddressName">南十里居</label>
+                          <label htmlFor="" className="itemAddressCode">3000</label>
+                      </p>
+                    </div>
+                </li>
+            </ul>
+            <div className="loadMore">
+              <button className="loadMoreBtn">加载更多</button>
+            </div>
         </div>
       </Layout>
     )
